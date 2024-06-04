582ed0bbb46c0be0fe5a1a41cab8a84ea1375b40
DevKipesa <kymdommie@gmail.com>
2024-06-04 14:53:16 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,16 @@ import './Footer.scss'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+        <div className='info'>
+            <h3 className='infor'>Pellentesque suscipit fringilla libo eu.</h3>
+            
+        </div>
+        <div className='foot'>
+
+        </div>
+    
+        </div>
     )
 }
 
