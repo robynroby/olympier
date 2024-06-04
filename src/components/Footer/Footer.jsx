979c06edbfe784f0aa7a5c2c3fe01f4b864a2980
DevKipesa <kymdommie@gmail.com>
2024-06-04 15:53:29 +0300

    few changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
-import './Footer.scss'
+import './Footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
@@ -8,7 +10,39 @@ const Footer = () => {
             
         </div>
         <div className='foot'>
-
+            <div className='copy'></div>
+        <div className='comp'>
+         <ul className='company'>
+         <h3>Company</h3>
+         <li>About us</li>
+         <li>Blog</li>
+         <li>Contact us</li>
+         <li>Pricing </li>
+         <li>Testimonials</li>
+            </ul> 
+            <ul className='support'>
+                <h3>Support</h3>
+                <li>Help Center</li>
+                <li>Terms of Service</li>
+                <li>Legal</li>
+                <li>Privacy Policy</li>
+                <li>Status</li>
+                </ul> 
+                <div>
+      <h3>Stay Up to Date</h3>
+      {/* <form onSubmit={handleSubmit}> */}
+        <div className="input-container">
+          <input
+            type="email"
+            placeholder="Your email address"
+            required
+          />
+          <button type="submit">
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </div>
+    </div>
+        </div>
         </div>
     
         </div>
