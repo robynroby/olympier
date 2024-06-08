32036d1ae5a858d9cc945ed862b7e0514e2df30c
DevKipesa <kymdommie@gmail.com>
2024-06-08 17:20:42 +0300

    Few changes of routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,29 @@ import Herosection from './components/Herosection/Herosection'
 import Navbar from './components/Navbar/Navbar'
 import OurClients from './components/OurClients/OurClients'
 import Services from './components/Services/Services'
+import Home from '../src/Home/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Herosection />
-      <CalltoAction />
-      <AboutUs />
-      <Services />
-      <OurClients />
-      <ContactUs />
-      <Footer />
-    </>
+    <Router>
+    <Navbar isVisible={true}  />
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  </>
+    
   )
 }
 
